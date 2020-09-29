def1e44db5d5fcda948d0f989c74080ab3ed6092
Mansouri Ayoub <mansouriayoub2.0@gmail.com>
2020-09-29 11:37:28 +0100

    add file to github repo

---
+++ app.js
@@ -0,0 +1,42 @@
+const correctAnswers = ['C', 'B', 'A', 'B'];
+const form = document.querySelector('#quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const userAnswers = [
+        form.q1.value,
+        form.q2.value,
+        form.q3.value,
+        form.q4.value,
+    ];
+    
+    let score = 0;
+    userAnswers.forEach((answer, index) => {
+        if (answer == correctAnswers[index]) {
+            score += 25;
+        }
+    })
+
+    // to make window goes up after submit
+    scrollTo(0, 0);
+
+    result.classList.remove('d-none');
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if (output === score) {
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+    }, 10);
+
+    // modal trigger
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').trigger('focus')
+    })
+})
+
